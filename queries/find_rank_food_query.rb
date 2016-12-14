58016c332e2031f86728dd4f3ada6deb98266b84
LeoLee <leo.li@iss.nthu.edu.tw>
2016-12-14 21:19:15 +0800

    fix bug

--- queries/find_rank_food_query.rb
+++ queries/find_rank_food_query.rb
@@ -2,10 +2,11 @@
 
 # Search query for postings in a group by optional keywords
 class FindRankFoodQuery
-  def self.call(RankCriteria)
-    if RankCriteria.degree
-      Popularity.order(:tag_amount).limit(RankCriteria.count).all
+  def self.call(criteria)
+    if criteria[:degree]
+      Popularity.order(:tag_amount).limit(criteria[:count]).all
     else
-      Popularity.reverse_order(:tag_amount).limit(RankCriteria.count).all
-  endr
+      Popularity.reverse_order(:tag_amount).limit(criteria[:count]).all
+    end
+  end
 end
