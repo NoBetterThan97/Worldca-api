8c30c71ddd8147370618d0af5c63e6203d8304c9
Cheng-Yu Hsu <m@cyhsu.me>
2017-01-05 20:12:00 +0800

    Clean up init.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-require 'rubygems'
-
 folders = 'config,controllers,lib,values,models,representers,queries,services'
 Dir.glob("./{#{folders}}/init.rb").each do |file|
   require file
